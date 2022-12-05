c6ad63a50c599a72070c28f60617aa036085145d
leadattic <axelcapuozzo2gmail.com>
2022-12-05 15:37:09 +0100

    TODO fix

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 //TODO: <time> (start stopwatch at the beginning of running and stop at end, sout time)
-//TODO openInBrowser<https://www.example.com> (opens the https address in the standard browser) https://stackoverflow.com/questions/5226212/how-to-open-the-default-webbrowser-using-java
 public class Main {
     static String os = System.getProperty("os.name").toLowerCase();
     static String code = "";
